96a01fa9f27e091a5061f1b31b6a67d24c781f2a
bcarballa <braiscarballapadin@gmail.com>
2023-09-26 19:33:21 +0200

    Creada clase AlmacenCentral

---
+++ src/AlmacenCentral.java
@@ -0,0 +1,17 @@
+public class AlmacenCentral {
+    private int capacidad;
+
+    
+    public AlmacenCentral() {
+        this.capacidad = 200;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    
+}
